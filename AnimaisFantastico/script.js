19756e3a2d5de4542774a3fb74d739275b66e590
AdrielBegin <adriel.pemi@gmail.com>
2023-07-04 17:28:22 -0400

    feat: criando tab

---
+++ AnimaisFantastico/script.js
@@ -0,0 +1,22 @@
+
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+
+function activeTab(index) {
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    });
+    tabContent[index].classList.add('ativo');
+}
+
+// aqui tem um loop para cada item da tela
+
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index);
+    });
+});
+
+
