bf418587b0e6599b99a4ae720053f2e1695d6e4d
AdrielBegin <adriel.pemi@gmail.com>
2023-07-05 15:54:11 -0400

    feat: criando initAccordion

--- AnimaisFantastico/script.js
+++ AnimaisFantastico/script.js
@@ -1,22 +1,45 @@
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
 
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
 
-function activeTab(index) {    
-    tabContent.forEach((section) => {
-        section.classList.remove("ativo");
+    function activeTab(index) {
+        tabContent.forEach((section) => {
+            section.classList.remove("ativo");
+        });
+        tabContent[index].classList.add("ativo");
+    }
+
+    // aqui tem um loop para cada item da tela
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index);
+        });
     });
-    tabContent[index].classList.add("ativo");
 }
+initTabNav();
 
-// aqui tem um loop para cada item da tela
+function initAccordion() {
+    cosnt = accordionList = document.querySelectorAll('.js-accordion dt');
 
-tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        activeTab(index);
-    });
-});
+    const activeClass = 'ativo';
+
+    if (accordionList.length) {
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAcoordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAcoordion);
+        });
+    }
+}
+initAccordion();
 
 
